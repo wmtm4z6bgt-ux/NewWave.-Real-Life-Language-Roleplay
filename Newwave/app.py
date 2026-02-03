4ecdf0a159d6900a483404365d5e010ba77f2385
wmtm4z6bgt-ux <wmtm4z6bgt@privaterelay.appleid.com>
2026-02-03 13:04:46 +0000

    save my work

--- Newwave/app.py
+++ Newwave/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect
-from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -21,4 +20,5 @@ def add():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
+    
