04ff84576f1bbf7481b9a8dd731b5f8588a8483a
wmtm4z6bgt-ux <wmtm4z6bgt@privaterelay.appleid.com>
2026-02-14 09:32:06 +0000

    save all changes

--- Newwave/app.py
+++ Newwave/app.py
@@ -91,5 +91,7 @@ def register():
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
